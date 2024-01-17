7f638bb9c481a02def9fc19e419e8701159a7ff6
mauro.franceschini <mauro.franceschini@gmail.com>
2024-01-17 06:46:45 +0100

    test: for null.Handling type

---
+++ sort/order/null/handling_test.go
@@ -0,0 +1,70 @@
+package null_test
+
+import (
+	"encoding/json"
+	"github.com/maurofran/page/sort/order/null"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseHandling(t *testing.T) {
+	tests := map[string]struct {
+		value    string
+		expected null.Handling
+		err      error
+	}{
+		"native":      {"native", null.HandlingNative, nil},
+		"nulls_first": {"nulls_first", null.HandlingNullsFirst, nil},
+		"nulls_last":  {"nulls_last", null.HandlingNullsLast, nil},
+		"invalid":     {"invalid", null.HandlingNative, null.ErrInvalidHandling},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual, err := null.ParseHandling(test.value)
+			assert.ErrorIs(t, err, test.err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestNullHandling_MarshalText(t *testing.T) {
+	tests := map[string]struct {
+		fixture  null.Handling
+		expected string
+	}{
+		"native":      {null.HandlingNative, `"NATIVE"`},
+		"nulls_first": {null.HandlingNullsFirst, `"NULLS_FIRST"`},
+		"nulls_last":  {null.HandlingNullsLast, `"NULLS_LAST"`},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual, err := json.Marshal(test.fixture)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, string(actual))
+		})
+	}
+}
+
+func TestNullHandling_UnmarshalText(t *testing.T) {
+	tests := map[string]struct {
+		fixture  string
+		expected null.Handling
+		err      error
+	}{
+		"native":      {`"NATIVE"`, null.HandlingNative, nil},
+		"nulls_first": {`"NULLS_FIRST"`, null.HandlingNullsFirst, nil},
+		"nulls_last":  {`"NULLS_LAST"`, null.HandlingNullsLast, nil},
+		"invalid":     {`"INVALID"`, null.HandlingNative, null.ErrInvalidHandling},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			var actual null.Handling
+			err := json.Unmarshal([]byte(test.fixture), &actual)
+			assert.ErrorIs(t, err, test.err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
